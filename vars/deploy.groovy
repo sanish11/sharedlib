05ce463c13395cee7c9b430b53534bdeb60c64de
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-05 15:37:43 +0545

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,18 @@
+def deployWar(Map config) {
+    def warFile = config.WAR_FILE
+    def sshUsername = config.SSH_USERNAME
+    def sshHostname = config.SSH_HOSTNAME
+    def sshPort = config.SSH_PORT
+    def remoteDirectory = config.REMOTE_DIRECTORY
+    def remoteFilename = config.REMOTE_FILENAME
+
+    if (warFile.empty) {
+        error "WAR file not found!"
+    }
+
+    echo "Deploying WAR file: ${warFile}"
+
+    withCredentials([sshUserPrivateKey(credentialsId: 'private', keyFileVariable: 'SSH_KEY')]) {
+        bat "scp -P ${sshPort} -i C:\\Users\\Owner\\.ssh\\id_rsa \"${warFile}\" \"${sshUsername}@${sshHostname}:${remoteDirectory}\\${remoteFilename}\""
+    }
+}
