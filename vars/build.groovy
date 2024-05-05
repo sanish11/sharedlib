e39b3519999b354da06b4db36fe6e8ef3ff372ac
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-05 14:14:52 +0545

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,16 +1,15 @@
 def call(Map config) {
-    def pomFile = config.POM_FILE
+    def pomFilePath = config.POM_FILE
     def mavenHome = config.MAVEN_HOME
 
-if (pomFile == null || !pomFile.isFile()) {
-    error "Invalid POM file!"
-}
+    if (pomFilePath == null || !(new File(pomFilePath)).isFile()) {
+        error "Invalid POM file!"
+    }
 
     echo "Building project using Maven..."
-    
-    // Change to the directory containing the POM file and run Maven clean and package
-    dir(pomFile.getParent()) {
-    sh "${mavenHome}/bin/mvn clean package"
-}
 
+    // Change to the directory containing the POM file and run Maven clean and package
+    dir(new File(pomFilePath).getParent()) {
+        sh "${mavenHome}/bin/mvn clean package"
+    }
 }
