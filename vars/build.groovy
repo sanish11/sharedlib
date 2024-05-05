b9a958921ba4d5210da5da16d815e6c2a46fd8bd
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-05 11:56:42 +0545

    Create build.groovy

---
+++ vars/build.groovy
@@ -0,0 +1,15 @@
+def call(Map config) {
+    def pomFile = config.POM_FILE
+    def mavenHome = config.MAVEN_HOME
+
+    if (pomFile.empty) {
+        error "POM file not found!"
+    }
+
+    echo "Building project using Maven..."
+    
+    // Change to the directory containing the POM file and run Maven clean and package
+    dir(pomFile.parent) {
+        sh "${mavenHome}/bin/mvn clean package"
+    }
+}
