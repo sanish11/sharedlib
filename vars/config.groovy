94b436be5da32f3ceaab17d7a4d75dfc8fc5e225
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-06 14:33:56 +0545

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,17 +1,10 @@
 def getConfig() {
     def config = [
-        TOMCAT_HOME: 'C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0',
-        CATALINA_HOME: 'C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0',
-        TOMCAT_WEBAPPS: 'C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\webapps',
+       
         SSH_USERNAME: 'Administrator',
         SSH_HOSTNAME: '103.94.159.179',
         SSH_PORT: '22',
-        REMOTE_DIRECTORY: 'C:\\test',
-        REMOTE_FILENAME: 'kumari.war',
-      WAR_FILE: 'C:\\Program Files\\Jenkins\\Jenkins home\\workspace\\KUMARI\\kumari-backend\\kb-web\\target\\kb-web-0.0.1-SNAPSHOT.war', // Specify the path to your WAR file dynamically
-              POM_FILE: 'C:\\Program Files\\Jenkins\\Jenkins home\\workspace\\test\\kumari-backend\\pom.xml',
-                                 
-        VERBOSE: 'true'
+    
 
     ]
     return config
