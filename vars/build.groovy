640001ca71e46ec17c407777198a91d35d965c42
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-06 17:26:55 +0545

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,6 @@
 // Change to the directory containing the POM file and run Maven clean and package
 def call(Map config) {
-    def pomFilePath = config.POM_FILE
+    
     def mavenHome = config.MAVEN_HOME
     echo ":::: pomFilePath ::: ${pomFilePath}"
 
@@ -8,8 +8,7 @@ def call(Map config) {
 
     echo "Building project using Maven..."
 
-    // Execute Maven clean and package from the current working directory
-    dir(pomFilePath){
+   
         bat "\"${mavenHome}/bin/mvn\" clean package"
-    }
+    
 }
