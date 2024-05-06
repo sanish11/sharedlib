b8788fb49694fea0c8469a53815afbb1af32b62e
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-06 14:46:35 +0545

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -4,6 +4,8 @@ def getConfig() {
         SSH_USERNAME: 'Administrator',
         SSH_HOSTNAME: '103.94.159.179',
         SSH_PORT: '22',
+         POM_FILE: 'kumari-backend/pom.xml',
+        WAR_FILE: 'kumari-backend/kb-web/target/kb-web-0.0.1-SNAPSHOT.war',
     
 
     ]
