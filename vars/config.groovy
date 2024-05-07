887ae861c50db387c982d54a04d5c22f27f20109
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-07 14:55:06 +0545

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,10 +1,8 @@
 def getConfig() {
     def config = [
-       
         SSH_USERNAME: 'Administrator',
         SSH_HOSTNAME: '103.94.159.179',
         SSH_PORT: '22',
-        
  ]
     return config
 }
