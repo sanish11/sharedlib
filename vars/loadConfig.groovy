c8049c62aa68d7f2cdf62b3934a8002797c4c318
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-07 15:01:06 +0545

    Update loadConfig.groovy

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -1,4 +1,4 @@
 def call() {
-    def config = new ConfigSlurper().parse(new File("${libraryResource 'config.groovy'}").toURL())
+    def config = new ConfigSlurper().parse(new File("${libraryResource 'vars/config.groovy'}").toURL())
     return config
 }
