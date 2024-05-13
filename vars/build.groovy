c21f7e8703569e7db1658c7aad7a11d20272c9c8
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-13 13:10:31 +0545

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -10,6 +10,7 @@ def call(Map config) {
 
     // Execute Maven clean and package from the current working directory
     dir(pomFilePath){
-        sh "\"${mavenHome}/bin/mvn\" clean install" -X
+        sh "${mavenHome}/bin/mvn clean install -X"
+
     }
 }
