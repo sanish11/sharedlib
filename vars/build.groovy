58ca19168921dd4ef879dabc11c2565396d0e2f4
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-13 13:21:55 +0545

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,7 @@
+User
 // Change to the directory containing the POM file and run Maven clean and package
 def call(Map config) {
     def pomFilePath = config.POM_FILE
-    def javaHome = config.JAVA_HOME
     def mavenHome = config.MAVEN_HOME
     echo ":::: pomFilePath ::: ${pomFilePath}"
 
@@ -10,14 +10,8 @@ def call(Map config) {
     echo "Building project using Maven..."
 
     // Execute Maven clean and package from the current working directory
-    dir(pomFilePath) {
-        // Specify the Java tool to be used
-        tool name: 'jdk8', type: 'jdk'
+    dir(pomFilePath){
+        sh "${mavenHome}/bin/mvn clean install -X"
 
-        // Execute Maven clean and package using the specified Java version
-        sh """
-            export JAVA_HOME=${javaHome}
-            ${mavenHome}/bin/mvn clean install -X
-        """
     }
 }
