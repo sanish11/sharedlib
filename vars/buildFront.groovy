3a45a96da27386118460183295d59d46805aee5c
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-22 16:12:23 +0545

    Create buildFront.groovy

---
+++ vars/buildFront.groovy
@@ -0,0 +1,14 @@
+def call() {
+    def nodejs = tool name: 'node16', type: 'jenkins.plugins.nodejs.tools.NodeJSInstallation'
+    withEnv(["PATH+NODEJS=${nodejs}/bin"]) {
+        dir('everest-frontend') {
+            sh '''
+                rm -rf node_modules
+                echo "Installing npm packages"
+                npm install
+                echo "Running npm build"
+                npm run build
+            '''
+        }
+    }
+}
