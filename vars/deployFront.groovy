b101441b8918ecf26034244c7337e0b875e637ae
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-22 17:11:26 +0545

    Create deployFront.groovy

---
+++ vars/deployFront.groovy
@@ -0,0 +1,20 @@
+
+def call(Map config) {
+    def distDirectory = config.DIST_DIRECTORY
+    def sshUsername = config.SSH_USERNAME
+    def sshHostname = config.SSH_HOSTNAME
+    def sshPort = config.SSH_PORT
+    def remoteDirectory = config.REMOTE_DIRECTORY
+
+    if (!fileExists(distDirectory)) {
+        error "Dist directory not found: ${distDirectory}"
+    }
+
+    echo "Deploying dist directory: ${distDirectory}"
+
+    withCredentials([sshUserPrivateKey(credentialsId: 'private', keyFileVariable: 'SSH_KEY')]) {
+        sh """
+            rsync -avz -e "ssh -p ${sshPort}" "${distDirectory}/" "${sshUsername}@${sshHostname}:${remoteDirectory}/"
+        """
+    }
+}
