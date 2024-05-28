5bd89cdee20d6a03ecdc3c68c88ee374cd236e8b
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-28 17:36:45 +0545

    Update deployFront.groovy

--- vars/deployFront.groovy
+++ vars/deployFront.groovy
@@ -10,11 +10,11 @@ def call(Map config) {
     }
 
     echo "Deploying dist directory: ${distDirectory}"
-
+    
     sh "zip -r dist.zip ${distDirectory}"
 
     withCredentials([sshUserPrivateKey(credentialsId: 'private', keyFileVariable: 'SSH_KEY')]) {
-        sh "scp -r -P ${sshPort} dist.zip ${sshUsername}@${sshHostname}:${remoteDirectory}"
-        sh "ssh -p ${sshPort} ${sshUsername}@${sshHostname} \"powershell -Command Expand-Archive -Path '${remoteDirectory}/dist.zip' -DestinationPath '${remoteDirectory}'\""
+         sh "scp -r -P 22 dist.zip Administrator@103.94.159.179:C:/test"
+         sh "ssh -p 22 Administrator@103.94.159.179 \"powershell -Command Expand-Archive -Path 'C:\\\\test\\\\dist.zip' -DestinationPath 'C:\\\\test\\\\'\""
     }
 }
