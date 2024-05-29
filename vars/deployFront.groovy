f7bee389803f36873046eb0d518945078114654e
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-05-29 11:58:30 +0545

    Update deployFront.groovy

--- vars/deployFront.groovy
+++ vars/deployFront.groovy
@@ -11,12 +11,17 @@ def call(Map config) {
 
     echo "Deploying dist directory: ${distDirectory}"
 
-    sh """
+sh """
         cd ${distDirectory}
-        zip -r dist.zip .
+        zip -r dist.zip ./*
     """
 
     withCredentials([sshUserPrivateKey(credentialsId: 'private', keyFileVariable: 'SSH_KEY')]) {
+         // Clean the remote directory
+        sh """
+            ssh -p ${sshPort} ${sshUsername}@${sshHostname} \"powershell -Command \"Remove-Item -Path '${remoteDirectory}\\*' -Recurse -Force\" \"
+        """
+        
          sh "scp -r -P 22 dist.zip Administrator@103.94.159.179:C:/test"
          sh "ssh -p 22 Administrator@103.94.159.179 \"powershell -Command Expand-Archive -Path 'C:\\\\test\\\\dist.zip' -DestinationPath 'C:\\\\test\\\\'\""
     }
