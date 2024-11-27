9e26f74ab0ef5cab000f100ae3d080ae7ae5240f
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-11-27 14:45:28 +0545

    Update buildFront.groovy
    front changes

--- vars/buildFront.groovy
+++ vars/buildFront.groovy
@@ -7,7 +7,7 @@ def call() {
                 echo "Installing npm packages"
                 npm install
                 echo "Running npm build"
-                npm run build
+                node --max_old_space_size=4096 ./node_modules/@angular/cli/bin/ng build --prod
             '''
         }
     }
